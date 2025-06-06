77d04baf80223663aac6c2913eb95fad53680e69
[MrOrayoh] <[odhiambobran004@gmail.com]>
2025-06-06 09:15:29 +0300

    second commit

--- src/motorbike/views.py
+++ src/motorbike/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'motorbike/home.html')
+
+def login(request):
+    return render(request, 'motorbike/login.html')
+
+def register(request):
+    return render(request, 'motorbike/register.html')
